3c18ee814c115ae9f21024f178ff1b1f6aa4169a
user9 <user9@kali>
2024-02-05 16:28:10 +0300

    initial page added

---
+++ lib/Screen/initial_pages/initial_page.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class InitialPage extends StatelessWidget {
+  const InitialPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Stack(
+       children: [
+         Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage('assets/images/truck.png')
+            )
+          ),
+        ),
+        Container(
+          color: Color.fromARGB(206, 0, 0, 0),
+        ),
+       ],
+    ),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+            Text('Welcome to Lorem', style:
+             TextStyle(
+              color: Color.fromARGB(235, 237, 237, 237),
+              fontWeight: FontWeight.bold,
+              fontSize: 24,
+              decoration: TextDecoration.none
+             ),),
+             Column(children: [
+              Container(
+                height: 50,
+                width: double.maxFinite,
+                margin: EdgeInsets.only(left: 20,right: 20, top: 10, bottom: 10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Color.fromARGB(78, 255, 255, 255),
+                ),
+                child: Center(
+                  child: Text('Sign Up',style:
+                               TextStyle(
+                                color: Color.fromARGB(235, 237, 237, 237),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                decoration: TextDecoration.none
+                               ), )
+                ),
+              ),
+              Container(
+                height: 50,
+                width: double.maxFinite,
+                margin: EdgeInsets.only(left: 20,right: 20, top: 10, bottom: 10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Color.fromARGB(78, 255, 255, 255),
+                ),
+                child: Center(
+                  child: Text('Sign In',style:
+                               TextStyle(
+                                color: Color.fromARGB(235, 237, 237, 237),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                decoration: TextDecoration.none
+                               ), )
+                ),
+              ),
+           
+             ],),
+                Text('join as a guest', style:
+             TextStyle(
+              color: Color.fromARGB(235, 237, 237, 237),
+              fontWeight: FontWeight.bold,
+              fontSize: 14,
+              decoration: TextDecoration.none
+             ),)
+        ],
+      )
+      ]);
+    
+    
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:towtruck_app/Screen/initial_pages/initial_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        
+        //
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: InitialPage()
+    );
+  }
+}
+
+
+
